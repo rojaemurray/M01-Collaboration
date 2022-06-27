a3455f8e6b5614faf7793b3e3f9e6b6ec079c194
rojaemurray <107898197+rojaemurray@users.noreply.github.com>
2022-06-27 01:00:30 -0400

    Add files via upload

---
+++ 4.2.py
@@ -0,0 +1,10 @@
+small = True
+green = False
+if small and green:
+    print("pea")
+elif small and not green:
+    print("cherry")
+elif not small and green:
+    print("watermelon")
+elif not small and not green:
+    print("pumpkin")
---
+++ 6.1.py
@@ -0,0 +1,4 @@
+Section= 6.2
+list=[3,2,1,0]
+for x in list:
+    print(x)
